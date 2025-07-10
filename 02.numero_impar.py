8c1b73ae5d70a70724f6ca8ef5cdf42209ec4044
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-10 17:48:54 -0300

    Update 02.numero_impar.py

--- 02.numero_impar.py
+++ 02.numero_impar.py
@@ -1,1 +1,1 @@
-# utilizando a estrutura condicional simples, verifique se um número é ímpar e, caso seja, escreva na tela "o número é ímpar".
+# Utilizando a estrutura condicional simples, verifique se um número é ímpar e, caso seja, escreva na tela "o número é ímpar".
