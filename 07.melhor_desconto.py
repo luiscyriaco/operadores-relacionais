11bf36b35cb4698a634ef62d70cf6d8014920fe1
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-11 20:59:44 -0300

    Create 07.melhor_desconto.py

---
+++ 07.melhor_desconto.py
@@ -0,0 +1,4 @@
+# Uma livraria está fazendo uma promoção para pagamento à vista em que o comprador pode escolher entre dois critérios de desconto:
+# a) Critério A: R$ 0,25 por livro + R$ 7,50 fixo
+# a) Critério B: R$ 0,50 por livro + R$ 2,50 fixo
+# Fazer umm programa em que o usuário digite a quantidade de livros que deseja comprar e o programa diga qual é a melhor opção de desconto.
