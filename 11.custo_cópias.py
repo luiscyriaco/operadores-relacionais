fd96ba884438666c96ec6cf0458cff99a85ac75c
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-11 21:30:21 -0300

    Create 11.custo_cópias.py

---
+++ 11.custo_cópias.py
@@ -0,0 +1,1 @@
+# Escrever um programa que resolva o seguinte problema: uma fotocópia custa R$ 0,25 por folha, mas acima de 100 folhas esse valor cai para R$ 0,20 por unidade. Dado o total de cópias, informe o valor a ser pago.
