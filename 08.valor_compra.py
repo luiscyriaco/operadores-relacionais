e81293b378625efa38f7b78753974b344ab16674
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-11 21:13:30 -0300

    Create 08.valor_compra.py

---
+++ 08.valor_compra.py
@@ -0,0 +1,8 @@
+# Considerar a situção em que um cliente faz uma determinada compra em uma loja. Ao realizar o pagamento, são oferecidas as seguintes condições para pagamento:
+# Pagamento à vista: 15% de desconto sobre o valor total da compra.
+# Pagamento com cheque pré-datado para 30 dias: 10% de desconto sobre o valor total da compra.
+# Pagamento parcelado em 3 vezes: 5% de desconto sobre o valor total da compra.
+# Pagamento parcelado em 6 vezes: não tem desconto.
+# Pagamento parcelado em 12 vezes: 8% de acréscimo sobre o valor total da compra.
+# De acordo com o valor total da compra, verificar a opção de pagamento do cliente, calcular também o valor das parcelas. Apresentar ao usuário uma mensagem com o valor total da compra
+a, o valor final da compra, a diferença entre os dois, identificar como desconto se a diferença for positiva, como juros se for negativa, mostrar, também, a quantidade e o valor das parcelas.
