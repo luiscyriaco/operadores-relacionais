8746a718ed45d5e727069beadca1e3dc8bc62a18
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-11 21:26:49 -0300

    Create 10.peso_ideal.py

---
+++ 10.peso_ideal.py
@@ -0,0 +1,3 @@
+# Elaborar um programa que verifique se o paciente está acima de seu peso ideal de acordo com a condição a seguir:
+# Para homens: (72.7*altura) - 58
+# Para mulheress: (62.1*altura) - 44.7
