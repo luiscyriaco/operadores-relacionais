0751f2d62664b507b7ad168456c99009c908628a
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-12 00:24:59 -0300

    Update 01.numero_par.py

--- 01.numero_par.py
+++ 01.numero_par.py
@@ -1,1 +1,12 @@
-# Utilizando a estrututa condicional simples, verifique se um número é par e, caso seja, escreva na tela "o número é par".
+""" 01. Utilizando a estrututa condicional simples, verifique se um número é par e, caso seja,
+escreva na tela "o número é par"
+"""
+
+print("Verificando se o número é par...")
+
+# solicitando um número inteiro ao usuário
+numero = int(input("Digite um número: "))
+
+# verificando se o número é par
+if numero % 2 == 0:
+    print("O número ",numero," é par")
