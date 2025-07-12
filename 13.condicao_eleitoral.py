c8d644b4164df52cf229c4f82f1d9aebf96df6b0
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-11 21:38:25 -0300

    Create 13.condicao_eleitoral.py

---
+++ 13.condicao_eleitoral.py
@@ -0,0 +1,4 @@
+# Escrever um programa que solicite a idade do usuário e exiba sua condição eleitoral:
+# Entre 16 e 17 anos: voto opcional.
+# Entre 18 e 70 anos: voto obrigatório.
+# Acima de 70 anos: voto opcional.
