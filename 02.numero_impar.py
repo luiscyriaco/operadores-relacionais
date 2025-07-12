f227eb8160636e3f788948d2a341a61e8cc4c66e
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-12 02:11:36 -0300

    Update 02.numero_impar.py

--- 02.numero_impar.py
+++ 02.numero_impar.py
@@ -1,1 +1,11 @@
-# Utilizando a estrutura condicional simples, verifique se um número é ímpar e, caso seja, escreva na tela "o número é ímpar".
+""" 02. Utilizando a estrutura condicional simples, verifique se um número é ímpar e, caso seja, escreva na tela "o número é ímpar".
+"""
+
+print("Verificando se o número é ímpar...")
+
+# solicitando um número inteiro ao usuário
+numero = int(input("Digite um número: "))
+
+# verificando se o número é ímpar
+if numero % 2 != 0:
+    print("O número ",numero," é ímpar")
