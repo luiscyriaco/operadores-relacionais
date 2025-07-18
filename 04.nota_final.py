56ac1686112b6ecd3a6cc3c8f7d666cd73e2e2b5
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-18 04:09:58 -0300

    Update 04.nota_final.py

--- 04.nota_final.py
+++ 04.nota_final.py
@@ -1,4 +1,7 @@
-""" 04. Em uma escola, as notas de cada prova variam de 0 a 100. Para o cálculo da média do semestre, o aluno precisa fazer três provas. Fazer um programa que solicite o valor de cada prova, calcule a média aritmética delas e mostre se ele foi aprovado ou reprovado. Para ser aprovado, a média deve ser igual ou maior do que 50.
+""" 04. Em uma escola, as notas de cada prova variam de 0 a 100. Para o cálculo 
+da média do semestre, o aluno precisa fazer três provas. Fazer um programa que 
+solicite o valor de cada prova, calcule a média aritmética delas e mostre se ele 
+foi aprovado ou reprovado. Para ser aprovado, a média deve ser igual ou maior do que 50.
 
 Média = (Prova1 + Prova2 + Prova3) / 3.
 """
