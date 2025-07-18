094086c86446af13284f1a66fbc61960f5293295
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-18 04:10:26 -0300

    Update 03.par_impar.py

--- 03.par_impar.py
+++ 03.par_impar.py
@@ -1,4 +1,5 @@
-""" 03.Utilizando a estrutura condicional composta, verifique se um número é par ou ímpar e escreva na tela a mensagem correspondente, conforme os exercícios 1 e 2.
+""" 03.Utilizando a estrutura condicional composta, verifique se um número é par 
+ou ímpar e escreva na tela a mensagem correspondente, conforme os exercícios 1 e 2.
 """
 
 print("Verificando se o número é par ou ímpar...")
