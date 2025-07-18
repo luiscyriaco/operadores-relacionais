ff35f6e3e5592764b19af9b8527ccef8f8643af7
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-18 04:09:13 -0300

    Update 05.password.py

--- 05.password.py
+++ 05.password.py
@@ -1,1 +1,13 @@
-# Escrever um programa que teste se a senha informada é igual a "AC12". Se sim, exibir a mensagem "Senha correta" e, se não, exibir "Senha incorreta".
+""" 05. Escrever um programa que teste se a senha informada é igual a "AC12". 
+Se sim, exibir a mensagem "Senha correta" e, se não, exibir "Senha incorreta. """
+
+print("Acesso ao sistema")
+
+# solicitando a senha ao usuário
+senha = input("Digite sua senha: ")
+
+# validando senha do usuário
+if senha == "AC12":
+    print("Senha correta.")
+else:
+    print("Senha incorreta.")
