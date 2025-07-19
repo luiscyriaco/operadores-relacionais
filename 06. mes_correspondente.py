4238b8a7440e3d5ff1d5be3a612b148116789178
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-19 01:35:50 -0300

    Update 06. mes_correspondente.py

--- 06. mes_correspondente.py
+++ 06. mes_correspondente.py
@@ -1,4 +1,4 @@
-""" Solicitar ao usuário um número inteiro e exibir o mês correspondente a este 
+""" 06. Solicitar ao usuário um número inteiro e exibir o mês correspondente a este 
 número, sendo o número 1 o mês de janeiro e 12, o mês de dezembro. Para  valores 
 fora da faixa entre 1 e 12, o programa deve informar que não é um mês válido.
 """
