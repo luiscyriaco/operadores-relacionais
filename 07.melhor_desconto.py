d6c42ae0977d02215905066e66a8c36340d4c332
luiscyriaco <167550107+luiscyriaco@users.noreply.github.com>
2025-07-19 01:36:16 -0300

    Update 07.melhor_desconto.py

--- 07.melhor_desconto.py
+++ 07.melhor_desconto.py
@@ -1,4 +1,4 @@
-""" Uma livraria está fazendo uma promoção para pagamento à vista em que o comprador 
+""" 07. Uma livraria está fazendo uma promoção para pagamento à vista em que o comprador 
 pode escolher entre dois critérios de desconto:
  a) Critério A: R$ 0,25 por livro + R$ 7,50 fixo
  b) Critério B: R$ 0,50 por livro + R$ 2,50 fixo
